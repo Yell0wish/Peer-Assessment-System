a1f8f00983f4b23133548c8f6ac2821a61ea7953
Yellowish <3104812631@qq.com>
2023-11-30 19:35:14 +0800

    修复了查重bug

--- src/main/java/com/ye/controller/ContentSimilarityCheckController.java
+++ src/main/java/com/ye/controller/ContentSimilarityCheckController.java
@@ -11,6 +11,7 @@ import com.ye.pojo.ClassPojo;
 import com.ye.pojo.FilePojo;
 import com.ye.pojo.UserPojo;
 import com.ye.utils.FileConversionUtil;
+import com.ye.utils.RandomProduct;
 import com.ye.utils.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,38 +22,61 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class ContentSimilarityCheckController {
+    // 定义一个对象用于作为锁
+    private static final Object lock = new Object();
+    private static String password = "";
     @RequestMapping(value = "/ContentCheck", method = RequestMethod.POST)
-    public String ContentCheck(HttpServletResponse response,
-                               @RequestParam("fileA") MultipartFile fileA,
+    public String ContentCheck(@RequestParam("fileA") MultipartFile fileA,
                                @RequestParam("fileB") MultipartFile fileB) throws IOException, InterruptedException {
-        // 只允许pdf word excel txt rtf文件
-        if (fileA.getContentType() == null && !(fileA.getContentType().contains("pdf") || fileA.getContentType().contains("word") || fileA.getContentType().contains("excel") || fileA.getContentType().contains("txt") || fileA.getContentType().contains("rtf"))) {
-            return Result.defeat("文件A格式不正确");
+        synchronized (lock) {        // 只允许pdf word excel txt rtf文件
+            if (fileA.getContentType() == null || !(fileA.getContentType().contains("pdf") || fileA.getContentType().contains("word") || fileA.getContentType().contains("excel") || fileA.getContentType().contains("txt") || fileA.getContentType().contains("rtf"))) {
+                return Result.defeat("文件A格式不正确, 只允许pdf word excel txt rtf文件");
+            }
+            if (fileB.getContentType() == null || !(fileB.getContentType().contains("pdf") || fileB.getContentType().contains("word") || fileB.getContentType().contains("excel") || fileB.getContentType().contains("txt") || fileB.getContentType().contains("rtf"))) {
+                return Result.defeat("文件B格式不正确, 只允许pdf word excel txt rtf文件");
+            }
+            String localPathA = "C:\\Users\\Yellowish\\Desktop\\new\\testA";
+            String localPathB = "C:\\Users\\Yellowish\\Desktop\\new\\testB";
+            String localResult = "C:\\Users\\Yellowish\\Desktop\\new\\result";
+            String localZipResult = "C:\\Users\\Yellowish\\Desktop\\new\\result.zip";
+            String fileAPath = localPathA + "\\" + fileA.getOriginalFilename();
+            String fileBPath = localPathB + "\\" + fileB.getOriginalFilename();
+            FileConversionUtil.deleteFolderContents(new File(localResult));
+            FileConversionUtil.deleteFolderContents(new File(localPathA));
+            FileConversionUtil.deleteFolderContents(new File(localPathB));
+            FileConversionUtil.saveMultipartFileToLocal(fileA, fileAPath);
+            FileConversionUtil.saveMultipartFileToLocal(fileB, fileBPath);
+
+
+            FileConversionUtil.deleteFolderContents(new File(localResult));
+            CheckManager.INSTANCE.setRegCode("D34auBYNDhIuC5PRqI75ItmgMjmtrSGQHFqutQtUQuJ2XDoSWZwdd28SQHW3iZz/gkAwO/Y5Y519XtXlZqCkBNLlCKjThrVDyC7Wkg7gA8s/f9lXMZFXcuq/6xPD/hUEifz/KggBWzEnnKXuXDrDWAOtqEEN7GiL1qcWALTANtypiuOHNc75wKnCITPjp8igQJiC/nco6WuiJa3JQhvPcQ3pX9I1Hv6wKiCmbAUGEihLHyMcnY3xCir1bRUUznhYmKl1h8ACVB3Cgbv18FN+AsAeFrehmaPF7FqMZvWjpM+dCa8KKLGmDCZH6k1mP2tre9dLGA0ar9YLl/K3OHr8f6ojJdp2xkAWuDrNKt0KzVqcsbLR/x/F/8cpGhkDkVJgC5AT6gl4FdV8YmLu5bRivlQo/FwSM+5itFhyDFd+yGjcWUXeA31d2UDoQ5wy33WxyLwQm3H7P6dU6rTqc2fqy98uToQZesMe2sBoU3EAUfE=");
+            EasyStarter.check(new File(localPathA), new File(localPathB), localResult);
+            FileConversionUtil.zipFolder(localResult, localZipResult);
+
+            password = RandomProduct.getCheckCode();
+            Map<String, Object> map = new HashMap<>();
+            map.put("token", password);
+            return Result.success("成功查重", map);
         }
-        if (fileB.getContentType() == null && !(fileB.getContentType().contains("pdf") || fileB.getContentType().contains("word") || fileB.getContentType().contains("excel") || fileB.getContentType().contains("txt") || fileB.getContentType().contains("rtf"))) {
-            return Result.defeat("文件B格式不正确");
+
+    }
+
+    @RequestMapping(value = "/getContentCheck", method = RequestMethod.GET)
+    public String getContentCheck(HttpServletResponse response,
+                                  @RequestParam("checkToken") String checkToken) throws IOException, InterruptedException {
+        if (!checkToken.equals(password)) {
+            return Result.defeat("checkToken不正确");
         }
-        String localPathA = "C:\\Users\\Yellowish\\Desktop\\new\\testA";
-        String localPathB = "C:\\Users\\Yellowish\\Desktop\\new\\testB";
-        String localResult = "C:\\Users\\Yellowish\\Desktop\\new\\result";
-        String localZipResult = "C:\\Users\\Yellowish\\Desktop\\new\\result.zip";
-        String fileAPath = localPathA + "\\" + fileA.getOriginalFilename();
-        String fileBPath = localPathB + "\\" + fileB.getOriginalFilename();
-        FileConversionUtil.deleteFolderContents(new File(localResult));
-        FileConversionUtil.deleteFolderContents(new File(localPathA));
-        FileConversionUtil.deleteFolderContents(new File(localPathB));
-        FileConversionUtil.saveMultipartFileToLocal(fileA, fileAPath);
-        FileConversionUtil.saveMultipartFileToLocal(fileB, fileBPath);
-
-
-        FileConversionUtil.deleteFolderContents(new File(localResult));
-        CheckManager.INSTANCE.setRegCode("wNapGy/uhgE+rv5BsW4gcSiL0qFHV1/xXVNbuKI91TdLKAKW7afYGrQD9YDRBQC6ixvYQuMDj2iBNF4CQ+oLWcWWQBV+kSpX/s788zBOvN7vfhdP/L0Neu17B3dulsPT3cMp0TzSSzOBRsCGA5IE5ipAdFLczrREL0UEdjxJivok1HNzTdllFHvw4Nxnm7iMp/H/g6T28QzQPhV2rNRxZ6fBZMPcXzpCf/Uk9fnh/XBCkFXEypcPZV65/mY5lMTA9KgK8MrcQmt63ARYx+P5GjMBI5Lvob9YCiQF4iGdi5S2+tmciqrOpnb7uZ8//XQoXTisxgLWAP40rT7/mmSsQ54Y3V1op2I5iU7wMMCIpklJC54TzTvXvPfmUcwx+lqZQ1EpPNoHQ9hbJq/05EgIgw/lJM2vUAbyHHfz8ShsfeGRJXQjIIxS1Sil3XMBABWp4buR34onla9RICq60+BthP9SaB8unCQ2S0ymxL9DRUo=");
-        EasyStarter.check(new File(localPathA), new File(localPathB), localResult);
-        return Result.success("result.zip", FileConversionUtil.zipFolder(localResult), response);
-
+        byte[] bytes = Files.readAllBytes(Paths.get("C:\\Users\\Yellowish\\Desktop\\new\\result.zip"));
+        return Result.success("result.zip", bytes, response);
     }
 
 
